30e6775df3068f85f228e17df5aa61c305f5b625
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-05-03 10:17:49 +0300

    update

--- task.js
+++ task.js
@@ -1,9 +1,18 @@
 
-function slugify(title) {
-   const slug = title.toLowerCase().replace(/\s+/g, "-");
-return slug;
+function makeArray(firstArray, secondArray, maxLength) {
+   let newArray = firstArray.concat(secondArray); 
+  
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength); 
+  } else {
+    return newArray; 
+  }
 }
-console.log(slugify("Arrays for begginers")); 
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript")); 
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
