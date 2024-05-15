b3316201b387a6392974327b30c3413b5bf6b655
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-05-15 17:19:40 +0300

    update

--- task.js
+++ task.js
@@ -1,18 +1,74 @@
 
-function makeArray(firstArray, secondArray, maxLength) {
-   let newArray = firstArray.concat(secondArray); 
-  
-  if (newArray.length > maxLength) {
-    return newArray.slice(0, maxLength); 
-  } else {
-    return newArray; 
-  }
-}
+// function isEnoughCapacity(products, containerSize) {
+//   let valuesTotal = 0;
+//   const productsValues = Object.values(products);
+//   for (const product of productsValues) {
+//     valuesTotal += product;
+//   }
+//   if (valuesTotal <= containerSize) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+// console.log(
+//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+// ); // false
+
+// console.log(
+//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+// ); // true
+
+// console.log(
+//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+// ); // false
+
+
+// function calcAverageCalories(days) {
+//   if (days.length === 0) {
+//     return 0;
+//   }
+//   let totalCalories = 0;
+//   for (const values of days) {
+//   totalCalories += values.calories;
+//   }
+//   const averageCalories = totalCalories / days.length;
+//   return averageCalories;
+
+// }
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 3010 },
+//     { day: "tuesday", calories: 3200 },
+//     { day: "wednesday", calories: 3120 },
+//     { day: "thursday", calories: 2900 },
+//     { day: "friday", calories: 3450 },
+//     { day: "saturday", calories: 3280 },
+//     { day: "sunday", calories: 3300 }
+//   ])
+// ); // 3180
+
+// console.log(
+//   calcAverageCalories([
+//     { day: "monday", calories: 2040 },
+//     { day: "tuesday", calories: 2270 },
+//     { day: "wednesday", calories: 2420 },
+//     { day: "thursday", calories: 1900 },
+//     { day: "friday", calories: 2370 },
+//     { day: "saturday", calories: 2280 },
+//     { day: "sunday", calories: 2610 }
+//   ])
+// ); // 2270
+
+// console.log(
+//   calcAverageCalories([])
+// ); // 0
+
+
