62aa4f48cc5bf5d2f6ac02e53b6f0a73378dfe81
Tanya Kotvytska <tanyakotvitskaya20@gmail.com>
2024-06-04 11:46:43 +0300

    new code

--- task.js
+++ task.js
@@ -1,34 +1,42 @@
-class StringBuilder {
-  #value;
-
-   constructor(params) {
-    this.#value = params;
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
   }
+];
 
-  getValue() {
-    return this.#value;
-  }
 
-  padEnd(str) {
-    this.#value += str;
-  }
+const listGallery = document.querySelector(".gallery");
+const fragment = document.createDocumentFragment();
 
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
+images.forEach(image => {
+    const li = document.createElement('li');
+    li.classList.add("list-item-img");
+    const img = document.createElement('img');
+    img.src = image.url;
+    img.alt = image.alt;
+    li.append(img);
+    fragment.append(li);
+});
 
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); 
-builder.padStart("^");
-console.log(builder.getValue()); 
-builder.padEnd("^");
-console.log(builder.getValue()); 
-builder.padBoth("=");
-console.log(builder.getValue()); 
+listGallery.append(fragment);
